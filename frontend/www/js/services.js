78519550b5ed9761394e2b66bc9bb1efce3e8e03
wandytuasimatupang <simatupangwandy@gmail.com>
2016-05-22 14:49:53 +0700

    Update Services Kategori

--- frontend/www/js/services.js
+++ frontend/www/js/services.js
@@ -1,5 +1,15 @@
 var app = angular.module('App.services', [])
 
+
+app.factory('kategoriService', function ($http) {
+	var baseUrl = 'http://localhost/webservice/server/kategori/';
+	return {
+		getAll: function () {
+			return $http.get(baseUrl + 'select_kategori.php');
+		},
+	};
+})
+
 app.service('AuthService', function ($http) {
 	var LOCAL_TOKEN_KEY = 'yourTokenKey';
 	var username = '';
