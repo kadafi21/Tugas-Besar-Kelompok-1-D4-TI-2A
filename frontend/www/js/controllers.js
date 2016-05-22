6116d0d04b255c728cedb158fa9dc232448d39e6
wandytuasimatupang <simatupangwandy@gmail.com>
2016-05-22 14:46:23 +0700

    Update Controller Home

--- frontend/www/js/controllers.js
+++ frontend/www/js/controllers.js
@@ -84,3 +84,20 @@ app.controller('LoginCtrl', function ($scope, $state, $ionicPopup, $ionicModal, 
 		});
 	}
 })
+
+app.controller('HomeCtrl', function ($scope, $state, kategoriService) {
+	$scope.title = "Toko Online";
+
+	$scope.showData = function () {
+		kategoriService.getAll().success(function (data) {
+			$scope.kategori = data;
+		}).finally(function () {
+			$scope.$broadcast('scroll.refreshComplete');
+		});
+	};
+	$scope.showData();
+
+	$scope.reload = function () {
+		$state.go('app.home');
+	};
+})
